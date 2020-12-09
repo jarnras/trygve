cba9c1c1d51bbd92c45f56f92e3b87a0d65cb008
Rasmus Järnström <rasmus.jarnstrom@columbiaroad.com>
2020-12-09 17:21:12 +0200

    Add nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,30 +8,49 @@ import { redirectToLogin } from '../../modules/admin/actions';
 class Header extends React.Component {
   static propTypes = {
     loggedIn: PropTypes.bool.isRequired,
+    location: PropTypes.object,
     redirectToLogin: PropTypes.func.isRequired,
   };
 
+
   render() {
+    const { pathname } = this.props.location || { pathname: '/' };
+
     return (
-      <div className="navbar navbar-default">
+      <nav className="navbar navbar-collapse navbar-default" id="navbarNav">
         <div className="container">
-          <a onClick={() => browserHistory.push(`${PREFIX_URL}/`)} className="navbar-brand">
+          <a onClick={() => browserHistory.push(`/`)} className="navbar-brand">
             {' '}
             {BRANDING_HEADER_TITLE}
           </a>
+            <ul className="nav navbar-nav nav-pills">
+              <li className={`nav-item ${pathname === "/" ? 'active' : ''}`}>
+                <a onClick={() => browserHistory.push(`/`)}> Hem </a>
+              </li>
+              <li className={`nav-item ${pathname === "/admin" ? 'active' : ''}`}>
+                <a onClick={() => browserHistory.push(`/admin`)}> Evenemang </a>
+              </li>
+              <li className={`nav-item ${pathname === "/admin/users" ? 'active' : ''}`}>
+                <a onClick={() => browserHistory.push(`/admin/users`)}> Användare </a>
+              </li>
+              <li className={`nav-item ${pathname === "/admin/points" ? 'active' : ''}`}>
+                <a onClick={() => browserHistory.push(`/admin/points`)}> Kvartettkonto </a>
+              </li>
+            </ul>
+
           {this.props.loggedIn ? (
             <a
-              onClick={() => this.props.redirectToLogin()}
-              className="navbar-brand"
-              style={{ float: 'right' }}
+            onClick={() => this.props.redirectToLogin()}
+            className="navbar-brand"
+            style={{ float: 'right' }}
             >
               Logout
             </a>
           ) : (
             ''
-          )}
-        </div>
-      </div>
+            )}
+            </div>
+        </nav>
     );
   }
 }
@@ -42,6 +61,7 @@ const mapDispatchToProps = {
 
 const mapStateToProps = state => ({
   loggedIn: state.admin.loggedIn,
+  location: state.location,
 });
 
 export default connect(
