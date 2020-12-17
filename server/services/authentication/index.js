94c9a8b7e6f92c552fdb9c44ab6772e096b5ab67
Rasmus Järnström <rasmus.jarnstrom@columbiaroad.com>
2020-12-17 18:29:09 +0200

    Heroku review apps

--- server/services/authentication/index.js
+++ server/services/authentication/index.js
@@ -5,28 +5,31 @@ const config = require('./../../../config/ilmomasiina.config.js');
 module.exports = function () {
   const app = this;
 
-  app.set('authentication', {
-    secret: config.feathersAuthSecret,
-    path: 'api/authentication',
-    service: 'api/users',
-    entity: 'user',
-    authStrategies: [
-      'local',
-      'jwt',
-    ],
-    jwtOptions: {
-      header: { typ: 'access' },
-      audience: 'http://localhost:3000',
-      issuer: 'feathers',
-      algorithm: 'HS256',
-      expiresIn: '1d',
-    },
-    local: {
-      entity: 'user',
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-  });
+
+  app.set('authentication', config.authenticationConfig);
+
+  // app.set('authentication', {
+  //   secret: config.feathersAuthSecret,
+  //   path: 'api/authentication',
+  //   service: 'api/users',
+  //   entity: 'user',
+  //   authStrategies: [
+  //     'local',
+  //     'jwt',
+  //   ],
+  //   jwtOptions: {
+  //     header: { typ: 'access' },
+  //     audience: 'http://localhost:3000',
+  //     issuer: 'feathers',
+  //     algorithm: 'HS256',
+  //     expiresIn: '1d',
+  //   },
+  //   local: {
+  //     entity: 'user',
+  //     usernameField: 'email',
+  //     passwordField: 'password',
+  //   },
+  // });
 
   const authService = new AuthenticationService(app);
 
